a30beea3fa7a5de6af4f97ef0c0072e8e666e7c9
kesia <kesia.salgado@gmail.com>
2025-08-05 19:37:32 -0300

    deploy cloud run

--- Dockerfile
+++ Dockerfile
@@ -30,8 +30,7 @@ COPY --from=build /app/dist ./dist
 
 # Expose the port your application listens on
 # Cloud Run expects your application to listen on the port specified by the PORT environment variable
-ENV PORT=3000
-EXPOSE ${PORT}
+EXPOSE 3000
 
 # Define the command to run your application
 # Assuming your built JavaScript entry point is in dist/index.js (adjust as needed)
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { transactions } from "./data";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
   res.json({ message: "Transactions API v1" });
@@ -11,8 +12,8 @@ app.get("/transactions", (_req, res) => {
   res.json({ transactions });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
 
 export default app;
